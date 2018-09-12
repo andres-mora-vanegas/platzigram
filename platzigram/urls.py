88d3be71c268db8d9837672d10edc3878959a606
Andres Mora Vanegas <andymora1907@hotmail.com>
2018-09-11 23:21:29 -0500

    [update] first class

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.urls import path
+from django.http import HttpResponse
+
+def hello_world(request):
+    return HttpResponse('hello, world')
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    #url(r'^admin/', admin.site.urls),
+    path('hello-world/',hello_world)
 ]
