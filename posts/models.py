2ad72668a912b9613a4c9a99df95c9d07dce37ac
andresmoradeveloper <andresmoradeveloper@gmail.com>
2018-10-05 17:41:56 -0500

    [update] update user Model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+
+    email = models.EmailField(max_length=254, unique=True)
+    password = models.CharField(max_length=50)
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    is_admin = models.BooleanField(default=False)
+
+    bio = models.TextField()
+
+    birthdate = models.DateField(blank=True,null=True)
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+
--- readme.md
+++ readme.md
@@ -1,12 +1,20 @@
 # inicio de proyecto
 
-``` django-admin startproject platzigram .```
+```sh
+ django-admin startproject platzigram .
+```
 
 ## instalación de django 2.1
-``` pip install django -U```
 
-## instalación applicación 
-``` python manage.py startapp posts ```
+```sh
+pip install django -U
+```
+
+## instalación applicación
+
+```sh
+python manage.py startapp posts
+```
 
 ## si django v3 instalar pip
 
@@ -15,6 +23,7 @@
 apt-get install python3-pip
 
 ```
+
 ## si django v3 instalar venv
 
 ```sh
